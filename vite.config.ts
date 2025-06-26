eafa3824cbb41d54e88dcf1dd33521e9baedc8f4
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2025-06-26 12:24:23 +0530

    TALENTIFY FRONTEND SETUP DONE  - Setup Tailwindcss for styling  - Setup tanstack router for routing  - Initialized shadecn in project  - Added biomejs for linting  - Added husky for pre commit hooks

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+      routesDirectory:'./src/app',
+      routeToken:'_layout'
+    }),
+    react({
+      babel: {
+        plugins: [
+          ['babel-plugin-react-compiler', {target:'19'}]
+        ]
+      }
+    }),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
